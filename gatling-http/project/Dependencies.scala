1a7560f8914e967b7e8004800b638ce34fef96aa
k.sosnovskiy <k.sosnovskiy@tinkoff.ru>
2022-03-28 15:59:59 +0400

    chore: up dependency

--- gatling-http/project/Dependencies.scala
+++ gatling-http/project/Dependencies.scala
@@ -4,14 +4,10 @@ object Dependencies {
   lazy val gatling: Seq[ModuleID] = Seq(
     "io.gatling.highcharts" % "gatling-charts-highcharts",
     "io.gatling"            % "gatling-test-framework"
-  ).map(_ % "3.7.5" % Test)
+  ).map(_ % "3.7.6" % Test)
 
   lazy val gatlingPicatinny: Seq[ModuleID] = Seq(
     "ru.tinkoff" %% "gatling-picatinny"
-  ).map(_ % "0.9.1")
-
-  lazy val janino: Seq[ModuleID] = Seq(
-    "org.codehaus.janino" % "janino"
-  ).map(_ % "3.1.2")
+  ).map(_ % "0.10.0")
 
 }
